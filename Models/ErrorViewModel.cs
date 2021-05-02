0ded03e62106eeb233f05676bea4eab42bea2e61
Deliriox <vidal-spartan@hotmail.com>
2021-05-01 22:03:49 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TipoCambio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
